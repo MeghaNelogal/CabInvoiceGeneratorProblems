c323cc310a432c011271bba6d6d0b72fc60ceb51
Your Name <you@example.com>
2022-05-29 13:09:13 +0530

    [Megha] Add: Calculating Aggregate Total For All Is Done

---
+++ CabInvoiceGeneratorProblem/InvoiceSummary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGeneratorProblem
+{
+    public class InvoiceSummary
+    {
+        //variables
+        public int totalNumberOfRides;
+        public double totalFair;
+        public double averageFair;
+        /// <summary>
+        /// using Constructore to Intialize the Intance Variable
+        /// </summary>
+        /// <param name="totalNumberOfRides"></param>
+        /// <param name="totalFair"></param>
+        public InvoiceSummary(int totalNumberOfRides, double totalFair)
+        {
+            this.totalNumberOfRides = totalNumberOfRides;
+            this.totalFair = totalFair;
+            this.averageFair = this.totalFair / this.totalNumberOfRides;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) ; return false;
+            if (!(obj is InvoiceSummary)) return false;
+            InvoiceSummary inputesObject = (InvoiceSummary)obj;
+            return this.totalNumberOfRides == inputesObject.totalNumberOfRides && this.totalFair == inputesObject.averageFair && this.averageFair == inputesObject.averageFair;
+        }
+        public override int GetHashCode()
+        {
+            return this.totalNumberOfRides.GetHashCode() ^ this.totalFair.GetHashCode() ^ this.averageFair.GetHashCode();
+        }
+    }
+}
+
--- CabInvoiceTest/UnitTest1.cs
+++ CabInvoiceTest/UnitTest1.cs
@@ -4,13 +4,21 @@ using NUnit.Framework;
 namespace CabInvoiceTest
 {
     public class Tests
-    {     
-        [Test]        
-            public void InputInInteger_ShouldReturn_TotalFair()
-            {
-                CabInvoiceGenerator invoice = new CabInvoiceGenerator(RideType.NORMAL);
-                double result = invoice.CalculatingFair(15, 5);
-                Assert.AreEqual(result, 250);
-            }
+    {
+        [Test]
+        public void InputInInteger_ShouldReturn_TotalFair()
+        {
+            CabInvoiceGenerator invoice = new CabInvoiceGenerator(RideType.NORMAL);
+            double result = invoice.CalculatingFair(15, 5);
+            Assert.AreEqual(result, 250);
+        }
+        [Test]
+        public void InputInInteger_ShouldReturn_MultipleRides_TotalFair()
+        {
+            CabInvoiceGenerator invoice = new CabInvoiceGenerator(RideType.PREMIER);
+            Ride[] rides = { new Ride(2, 3), new Ride(4, 5), new Ride(5, 6) };
+            double result = invoice.CalaulateMultipleRides(rides);
+            Assert.AreEqual(result, 46);
+        }
     }
 }
